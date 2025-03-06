773ea87b87483c515878399d31c382f9c7725080
@amritmohan <amritmohan2284@gmail.com>
2025-03-06 12:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(`Failed to load image: ${url}`);
+  });
+}
+
+async function downloadImages() {
+  output.innerHTML = "<p>Loading images...</p>";
+
+  try {
+    const imageElements = await Promise.all(images.map(img => downloadImage(img.url)));
+    output.innerHTML = ""; 
+    imageElements.forEach(img => output.appendChild(img));
+  } catch (error) {
+    output.innerHTML = `<p style="color:red;">${error}</p>`;
+  }
+}
+
+btn.addEventListener("click", downloadImages);
